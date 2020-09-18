7df7dd14347224eda598279a5d9566674e1aec17
David <davidjesus1284@gmail.com>
2020-09-17 21:44:15 -0500

    Fin seccion 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -27,7 +30,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
